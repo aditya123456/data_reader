2bffc097753658e3363a9a5559d5936dd38d5024
aditya <pratapsunips18@gmail.com>
2019-05-20 01:28:29 +0530

    updated'

--- EXCELLibray.py
+++ EXCELLibray.py
@@ -22,34 +22,42 @@ class EXCELLibray(object):
         print data
         return data
 
-    def Datareader(self, file_name, sheet_name, FLAG):
+    def Datareader(self, file_name,  FLAG):
         filter_list =[]
         book = open_workbook(file_name)
-        sheet = book.sheet_by_index(sheet_name)
+        sheet = book.sheet_by_index(0)
 
         # read header values into the list
-        keys = [sheet.cell(0, col_index).value for col_index in xrange(sheet.ncols)]
+        keys = [sheet.cell(0, col_index).value for col_index in range(sheet.ncols)]
         print keys
 
         dict_list = []
-        for row_index in xrange(1, sheet.nrows):
-            d = {keys[col_index]: sheet.cell(row_index, col_index).value
-                 for col_index in xrange(sheet.ncols)}
+        for row_index in range(1, sheet.nrows):
+            d = {keys[col_index]: sheet.cell(row_index, col_index).value for col_index in range(sheet.ncols)}
             dict_list.append(d)
+        print "before flag"
         print dict_list
         for i in dict_list:
             if i['executionFlag'] == FLAG:
                 filter_list.append(i)
+        print "after flag"
         print filter_list
-        print dict_list
+        # print dict_list
         return filter_list
 
-    def put_values_in_json(self, firstName, lastName, value1, value2):
-        sample_json = {"firstName":firstName, "lastName":lastName, "value1":value1, "value2":value2}
-        return sample_json
+    def put_values_in_json(self, email1):
+        sample_json1={  "id": 4, "email": "<email1>","first_name": "Eve", "last_name": "Holt", "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
+        }
+        for k, v in sample_json1.items():
+            if v == '<email1>':
+                sample_json1[k]=email1
+        print sample_json1
+        # sample_json = {"name1":{"firstName":firstName, "lastName":lastName, "value1":value1, "value2":value2},
+        #               "name2":{"firstName":firstName, "lastName":lastName, "value1":value1, "value2":value2}}
+        return sample_json1
 
 
 
 if __name__ == '__main__':
     x = EXCELLibray()
-    x.Datareader('sample.xlsx', 0, 'Y')
+    x.Datareader('sample.xlsx', 'Y')
--- execel.robot
+++ execel.robot
@@ -5,9 +5,11 @@ Library    RequestsLibrary
 
 *** Test Cases ***
 API Demo Post Request
-   ${all_values}=  READ EXCEL USING XLRD   sample.xlsx  Y
+   ${all_values}=  datareader   sample.xlsx   Y
    log  ${all_values}
+   ${json_request}=  put values in json  ${all_values}[0][email1]
+   log  ${json_request}
    :FOR   ${INDEX}  IN   @{all_values}
    \  LOG  ${INDEX}
-   \  ${json_request}=  put values in json  ${INDEX}[firstName]  ${INDEX}[lastName]  ${INDEX}[Value1]  ${INDEX}[Value2]
+   \  ${json_request}=  put values in json  ${INDEX}[email1]
    \  log  ${json_request}
