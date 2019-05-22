13e20fa28303bf87c9b0e0bf0b591fdd070f64b5
aditya <pratapsunips18@gmail.com>
2019-05-23 01:09:00 +0530

    update library

--- EXCELLibray.py
+++ EXCELLibray.py
@@ -1,9 +1,8 @@
-
 from xlrd import open_workbook
+import json
 
 class EXCELLibray(object):
 
-    
     def Datareader(self, file_name,  FLAG):
         filter_list =[]
         book = open_workbook(file_name)
@@ -17,26 +16,37 @@ class EXCELLibray(object):
         for row_index in range(1, sheet.nrows):
             d = {keys[col_index]: sheet.cell(row_index, col_index).value for col_index in range(sheet.ncols)}
             dict_list.append(d)
-        print "before flag"
-        print dict_list
+        print ("before flag")
+        print (dict_list)
         for i in dict_list:
             if i['executionFlag'] == FLAG:
                 filter_list.append(i)
-        print "after flag"
-        print filter_list
+        print ("after flag")
+        print (filter_list)
         # print dict_list
         return filter_list
 
-    def put_values_in_json(self, email1):
-        sample_json1={  "id": 4, "email": "<email1>","first_name": "Eve", "last_name": "Holt", "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
-        }
-        for k, v in sample_json1.items():
-            if v == '<email1>':
-                sample_json1[k]=email1
-        print sample_json1
-        # sample_json = {"name1":{"firstName":firstName, "lastName":lastName, "value1":value1, "value2":value2},
-        #               "name2":{"firstName":firstName, "lastName":lastName, "value1":value1, "value2":value2}}
-        return sample_json1
+
+    def put_values_in_json_with_json_file(self, file_name, index):
+        json_res=''
+        try:
+            json_res = json.loads(open(file_name).read())
+        except:
+            print ("file does not exist")
+        data = json_res['data']
+        try:
+            for i in data:
+                for json_key ,json_value in i.items():
+                    try:
+                        for excel_key,excel_value in index.items():
+                            if excel_key == json_value:
+                                i[json_key] = excel_value
+                    except:
+                        pass
+        except:
+            pass
+        print (json.dumps(json_res))
+        return json.dumps(json_res)
 
 
 
--- execel.robot
+++ execel.robot
@@ -6,9 +6,7 @@ Library    RequestsLibrary
 API Demo Post Request
    ${all_values}=  datareader   sample.xlsx   Y
    log  ${all_values}
-   ${json_request}=  put values in json  ${all_values}[0][email1]
-   log  ${json_request}
    :FOR   ${INDEX}  IN   @{all_values}
    \  LOG  ${INDEX}
-   \  ${json_request}=  put values in json  ${INDEX}[email1]
+   \  ${json_request}=  put values in json with json file  sample.json  ${INDEX}
    \  log  ${json_request}
